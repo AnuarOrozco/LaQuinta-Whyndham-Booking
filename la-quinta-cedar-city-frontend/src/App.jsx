a730d52b7117a092e11f1abbf2eea5a8a110dcf3
AnuarOrozco <anuarorozzco@gmail.com>
2025-05-12 19:00:28 -0600

    refactor(app): delete the imports from Header and Footer since we are implementing it inside Home

--- la-quinta-cedar-city-frontend/src/App.jsx
+++ la-quinta-cedar-city-frontend/src/App.jsx
@@ -1,12 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Footer from './components/Footer';
-import Header from './components/Header'; // Cambié Navbar por Header que creamos
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
-      <Header /> {/* Reemplazado Navbar por nuestro Header */}
       <main className="flex-grow">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -18,7 +15,6 @@ function App() {
           */}
         </Routes>
       </main>
-      <Footer />
     </div>
   );
 }
