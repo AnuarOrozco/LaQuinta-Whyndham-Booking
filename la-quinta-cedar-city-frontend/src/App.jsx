b5e549d94a0e8d3952e0dc17a15484c375ac3646
AnuarOrozco <anuarorozzco@gmail.com>
2025-05-12 18:54:11 -0600

    refactor: simplify App.jsx to only include Home page and new Header

--- la-quinta-cedar-city-frontend/src/App.jsx
+++ la-quinta-cedar-city-frontend/src/App.jsx
@@ -1,21 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Rooms from './pages/Rooms';
-import Amenities from './pages/Amenities';
-import Contact from './pages/Contact';
-import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Header from './components/Header'; // Cambié Navbar por Header que creamos
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
-      <Navbar />
+      <Header /> {/* Reemplazado Navbar por nuestro Header */}
       <main className="flex-grow">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/rooms" element={<Rooms />} />
-          <Route path="/amenities" element={<Amenities />} />
-          <Route path="/contact" element={<Contact />} />
+          {/* 
+            Componentes no utilizados actualmente (comentados):
+            <Route path="/rooms" element={<Rooms />} />
+            <Route path="/amenities" element={<Amenities />} />
+            <Route path="/contact" element={<Contact />} /> 
+          */}
         </Routes>
       </main>
       <Footer />
